9d7b234bda8888a905b59152943152f8f60bd1a5
gorgordon <cchiuac@connect.ust.hk>
2021-02-05 18:24:29 +0800

    mainls

---
+++ lab_0_code.ino
@@ -0,0 +1,13 @@
+char input = 0;
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    input = Serial.read();
+    Serial.print(input);
+  }
+  //Add a comment feature LED
+}
